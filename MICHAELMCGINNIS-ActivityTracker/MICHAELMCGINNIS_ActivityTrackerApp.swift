b451dd55cccef7d5d94cf418dbc38a0cc4b05fc8
Rukosa <mwmcginnis490@gmail.com>
2022-03-20 12:46:51 -0700

    Initial Commit

---
+++ MICHAELMCGINNIS-ActivityTracker/MICHAELMCGINNIS_ActivityTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MICHAELMCGINNIS_ActivityTrackerApp.swift
+//  MICHAELMCGINNIS-ActivityTracker
+//
+//  Created by Michael Mcginnis on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MICHAELMCGINNIS_ActivityTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
